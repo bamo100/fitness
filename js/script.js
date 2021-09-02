1b98fe2dbc40c601e477abeb79dc0f849fa096e9
bamo <bankolediamond10@gmail.com>
2021-09-02 22:19:14 +0100

    fit

---
+++ js/script.js
@@ -0,0 +1,21 @@
+// $(document).ready(function(){
+
+//     $('.img-respon').click(function(){
+//        $(this).toggleClass("active");
+//        $(".header").toggleClass("active");
+//     });
+// });
+
+
+function openmenu() {
+    document.getElementById("side-menu").style.display="block";
+    document.getElementById("menu-btn").style.display="none";
+    document.getElementById("close-btn").style.display="block";
+}
+function closemenu() {
+    document.getElementById("side-menu").style.display="none";
+    document.getElementById("menu-btn").style.display="block";
+    document.getElementById("close-btn").style.display="none";
+}
+
+ 
